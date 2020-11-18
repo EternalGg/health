5e58269e5b1aa650dc44de80ccd9267f9514c1e7
ym <1710164628@qq.com>
2020-11-18 10:35:40 +0800

    migrate

--- Health/User/models.py
+++ Health/User/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class user(models.Model):
+    u_account = models.CharField(max_length=20,unique=True)
+    u_password = models.CharField(max_length=20)
