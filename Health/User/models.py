ccfd6fe9a571078c9ca6ed7d6a30e659dcff47f9
ym <1710164628@qq.com>
2020-12-07 21:52:43 +0800

    migrate

--- Health/User/models.py
+++ Health/User/models.py
@@ -4,3 +4,10 @@ from django.db import models
 class user(models.Model):
     u_account = models.CharField(max_length=20,unique=True)
     u_password = models.CharField(max_length=20)
+    u_sex = models.CharField(max_length=20)
+    u_name = models.CharField(max_length=20)
+    u_phone = models.PhoneNumberField(max_length=11)
+    u_history = models.CharField(max_length=100)
+    u_birthday = models.DateTimeField()
+    
+
