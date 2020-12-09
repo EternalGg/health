3e54d1d5a7345c00b944ec11a0718797054b3947
Administrator <1710164628@qq.com>
2020-12-09 18:06:38 +0800

    。。。

--- Health/User/models.py
+++ Health/User/models.py
@@ -6,8 +6,8 @@ class user(models.Model):
     u_password = models.CharField(max_length=20)
     u_sex = models.CharField(max_length=20)
     u_name = models.CharField(max_length=20)
-    u_phone = models.PhoneNumberField(max_length=11)
+    u_phone = models.CharField(max_length=11,unique=True)
     u_history = models.CharField(max_length=100)
-    u_birthday = models.DateTimeField()
-    
+    u_age = models.IntegerField()
+    u_weight = models.FloatField()
 
